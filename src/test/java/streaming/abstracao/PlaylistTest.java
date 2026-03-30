9ddfbf86e4fd97e1f507e944c75bebaa122ed01e
LucasTSad <ddddesad@gmail.com>
2026-03-30 09:13:54 -0300

    Teste da classe Playlist

---
+++ src/test/java/streaming/abstracao/PlaylistTest.java
@@ -0,0 +1,28 @@
+package streaming.abstracao;
+
+import streaming.abstracao.Playlist;
+import streaming.implementacao.Spotify;
+import streaming.implementacao.AppleMusic;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaylistTest {
+
+    @Test
+    void spotifyDeveCalcularCustoExtraParaPlaylist() {
+        Playlist playlist = new Playlist(new Spotify(), "Chill Vibes", 10, 40.0);
+        assertTrue(playlist.getCustoExtra() > 0);
+    }
+
+    @Test
+    void appleMusicNaoDeveCobrarCustoExtraParaPlaylist() {
+        Playlist playlist = new Playlist(new AppleMusic(), "Chill Vibes", 10, 40.0);
+        assertEquals(0.0, playlist.getCustoExtra());
+    }
+
+    @Test
+    void duracaoTotalDeveEstarCorreta() {
+        Playlist playlist = new Playlist(new Spotify(), "Workout Mix", 12, 55.0);
+        assertEquals(55.0, playlist.getDuracaoTotal());
+    }
+}
